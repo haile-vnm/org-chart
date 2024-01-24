2288ffefe3dce835cba59b2e57012fbb030ad8c9
Hai Le <haile.vnm@gmail.com>
2024-01-24 11:38:06 +0700

    feat: switch to smoothstep connection

--- libs/org-chart/src/lib/org-chart.tsx
+++ libs/org-chart/src/lib/org-chart.tsx
@@ -25,23 +25,73 @@ const initNodes = [
     position: { x: -200, y: 200 },
   },
   {
+    id: '21',
+    type: 'custom',
+    data: { name: 'Tyler Weary', job: 'Designer', emoji: '🤓' },
+    position: { x: -400, y: 200 },
+  },
+  {
+    id: '22',
+    type: 'custom',
+    data: { name: 'Tyler Weary', job: 'Designer', emoji: '🤓' },
+    position: { x: -600, y: 200 },
+  },
+  {
     id: '3',
     type: 'custom',
     data: { name: 'Kristi Price', job: 'Developer', emoji: '🤩' },
     position: { x: 200, y: 200 },
+  },
+  {
+    id: '4',
+    type: 'custom',
+    data: { name: 'Kristi Price', job: 'Developer', emoji: '🤩' },
+    position: { x: 400, y: 200 },
+  },
+  {
+    id: '5',
+    type: 'custom',
+    data: { name: 'Kristi Price', job: 'Developer', emoji: '🤩' },
+    position: { x: 600, y: 200 },
   },
 ];
 
 const initEdges = [
   {
     id: 'e1-2',
+    type: 'smoothstep',
     source: '1',
     target: '2',
   },
   {
     id: 'e1-3',
+    type: 'smoothstep',
     source: '1',
     target: '3',
+  },
+  {
+    id: 'e1-4',
+    type: 'smoothstep',
+    source: '1',
+    target: '4',
+  },
+  {
+    id: 'e1-5',
+    type: 'smoothstep',
+    source: '1',
+    target: '5',
+  },
+  {
+    id: 'e1-21',
+    type: 'smoothstep',
+    source: '1',
+    target: '21',
+  },
+  {
+    id: 'e1-22',
+    type: 'smoothstep',
+    source: '1',
+    target: '22',
   },
 ];
 
